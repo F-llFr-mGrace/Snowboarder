66b6e9f9511b3cbde1a189b562ebe075ffc4f3f3
Grace Fowler <f.llfr.mgrace@gmail.com>
2023-09-25 16:04:36 -0400

    NameSpaces & SceneManagement || Using Invoke() for Delays
    I forgot to commit

--- Snowboarder/Assets/Scripts/Crash Detector.cs
+++ Snowboarder/Assets/Scripts/Crash Detector.cs
@@ -1,25 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float fltInvokeTime = .5f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Terrain"))
         {
-            Debug.Log("Ouch");
+            Invoke("ReloadScene", fltInvokeTime);
         }
     }
-    // Start is called before the first frame update
-    void Start()
+    void ReloadScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(0);
     }
 }
--- Snowboarder/Assets/Scripts/Finish Line.cs
+++ Snowboarder/Assets/Scripts/Finish Line.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float fltInvokeTime = .5f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag ("Player"))
         {
-            Debug.Log("Fin");
+            Invoke("ReloadScene", fltInvokeTime);
         }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
