ca411aa1f8574359e440410bd9111d582d243331
Grace Fowler <f.llfr.mgrace@gmail.com>
2023-10-01 22:53:27 -0400

    Switch to desktop

---
+++ Snowboarder/Assets/Scripts/Dust Trail.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustTrail : MonoBehaviour
+{
+    [SerializeField] ParticleSystem dustParticles;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision)
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        
+    }
+}
